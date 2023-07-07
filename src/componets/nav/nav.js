d415271e6454074f7209d050520d31a8e185881c
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-06 20:07:32 -0400

    added a little extraness

--- src/componets/nav/nav.js
+++ src/componets/nav/nav.js
@@ -2,11 +2,13 @@ import './nav.css';
 
 const Nav = props => 
     <nav id="nav">
+        <div id="first">
         <h1>Justin Delgado</h1>
-
+        <h4>Full Stack Web Developer</h4>
+        </div>
         <div>
             <button onClick={() => props.setPage('about')}>About Me</button>
-            <button onClick={() => props.setPage('work')}>Applications</button>
+            <button onClick={() => props.setPage('work')}>Projects</button>
             <button onClick={() => props.setPage('contact')}>Contact</button>
         </div>
     </nav>
--- src/componets/work/work.js
+++ src/componets/work/work.js
@@ -8,7 +8,7 @@ const Work = () => {
 
   return (
     <div id="work">
-      <h1>Applications</h1>
+      <h1>Recent Projects</h1>
 
       <div id="appsDiv">
         {apps.map(({ key, appName, image, info, websiteUrl }) => (
