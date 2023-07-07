17a916d2bee47e2871ad66775198d12ca4871fd5
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-07 15:01:36 -0400

    simple <br/> inputs

--- src/componets/about/about.jsx
+++ src/componets/about/about.jsx
@@ -7,17 +7,17 @@ const About = () =>
         <img src={me} alt="picture of me" />
         <h1>About me</h1>
     <h2> 
-     I'm Justin Delgado, a passionate full-stack developer who has honed my skills through the rigorous training I received at UCF's esteemed bootcamp. 
-        I've always had a deep love for technology, and being able to bring ideas to life through web development is my ultimate passion.
-    During my time at UCF's bootcamp, I immersed myself in learning everything I could about both front-end and back-end development.
+     I'm Justin Delgado, a passionate full-stack developer <br /> who has honed my skills through the rigorous training I received at UCF's esteemed bootcamp.<br /> 
+        I've always had a deep love for technology, and being able to bring ideas to life through web development <br /> is my ultimate passion.
+    During my time at UCF's bootcamp, I immersed myself in learning everything I could about both front-end and back-end development. <br />
      I've become well-versed in languages like 
-    HTML, CSS, JavaScript, and Python, and I've also gained expertise in frameworks and libraries such as React, Angular, and Django. 
-    This comprehensive knowledge allows me to tackle complex projects with confidence and deliver exceptional results. 
+    HTML, CSS, JavaScript, and Python, <br /> and I've also gained expertise in frameworks and libraries such as React, Angular, and Django. <br /> 
+    This comprehensive knowledge allows me to tackle complex projects with confidence and deliver exceptional results. <br />
     My portfolio is a testament to my creativity and 
-    attention to detail, showcasing a range of web applications and projects that combine stunning design with robust functionality. 
+    attention to detail, <br /> showcasing a range of web applications and projects that combine <br /> stunning design with robust functionality. <br /> 
     I'm always eager to stay up-to-date with the 
-    latest industry trends, ensuring that my work remains at the forefront of 
-    innovation in web development. With my dedication, skills, and unwavering commitment to excellence, 
+    latest industry trends, <br /> ensuring that my work remains at the forefront of 
+    innovation in web development. <br /> With my dedication, skills, unwavering commitment to excellence, <br />
     I'm ready to make a significant impact in the world of full-stack development.
         </h2>
         <div id="skills">
