536b628beeec2951c51e94fdbabe298712dcb022
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-09 20:30:37 -0400

    added a me id

--- src/componets/about/about.jsx
+++ src/componets/about/about.jsx
@@ -14,21 +14,17 @@ import skills10 from './images/op.png';
 const About = () =>
     <div id="about">
         <img src={me} alt="picture of me" />
+        <div id='me'>
         <h1>About me</h1>
-    <h2> 
+        <h2> 
      I'm Justin Delgado, a passionate full-stack developer <br /> who has honed my skills through the rigorous training I received at UCF's esteemed bootcamp.<br /> 
         I've always had a deep love for technology, and being able to bring ideas to life through web development <br /> is my ultimate passion.
     During my time at UCF's bootcamp, I immersed myself in learning everything I could about both front-end and back-end development. <br />
      I've become well-versed in languages like 
     HTML, CSS, JavaScript, and Python, <br /> and I've also gained expertise in frameworks and libraries such as React, Angular, and Django. <br /> 
-    This comprehensive knowledge allows me to tackle complex projects with confidence and deliver exceptional results. <br />
-    My portfolio is a testament to my creativity and 
-    attention to detail, <br /> showcasing a range of web applications and projects that combine <br /> stunning design with robust functionality. <br /> 
-    I'm always eager to stay up-to-date with the 
-    latest industry trends, <br /> ensuring that my work remains at the forefront of 
-    innovation in web development. <br /> With my dedication, skills, unwavering commitment to excellence, <br />
-    I'm ready to make a significant impact in the world of full-stack development.
         </h2>
+        </div>
+
         <div id="skills">
             <img src={skills1} alt='skills' />
             <img src={skills2} alt='skills' />
@@ -48,9 +44,6 @@ const About = () =>
         <p>Ipsa explicabo commodi, eveniet voluptatibus minus ratione sequi. Quisquam quos labore harum. Debitis, molestiae nemo amet laudantium perferendis dicta culpa libero voluptates vel non voluptate.</p>
         <p>Quisquam quos labore harum. Debitis, molestiae nemo amet laudantium perferendis dicta culpa libero voluptates vel non voluptate.</p>
         </div>
-        {/* <div id="skills">
-            <img src={skills1} alt='skills' />
-        </div> */}
     </div>
 
 export default About;
