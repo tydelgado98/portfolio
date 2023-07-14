e58670547de9738367ffa370ae1f271f15816e30
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-14 00:13:51 -0400

    made the link to github

--- src/componets/contact/contact.js
+++ src/componets/contact/contact.js
@@ -2,20 +2,22 @@ import './contact.css';
 import git from './images/Git.png';
 import linked from './images/linked.png';
 
-
-const Contact = () => 
-    <div id="contact">
-        <div id="links">
-        <h1>Email</h1>
-       <p>
-         <a href="mailto: delgadojustin100@yahoo.com">Delgadojustin100@yahoo.com</a>
-           </p>
-           </div>
-           <div id="click">
-           <img src={git} alt="git" />
-           <img src={linked} alt="linked" />
-           </div>
+const Contact = () => (
+  <div id="contact">
+    <div id="links">
+      <h1>Email</h1>
+      <p>
+        <a href="mailto:delgadojustin100@yahoo.com">Delgadojustin100@yahoo.com</a>
+      </p>
     </div>
-    
+    <div id="click">
+      <a href="https://github.com/tydelgado98">
+        <img src={git} alt="git" />
+        </a>
+        <img src={linked} alt="linked" />
+      
+    </div>
+  </div>
+);
 
 export default Contact;
