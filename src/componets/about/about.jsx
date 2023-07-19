6fe8b4aa6188f8c9af886f763f0151b31d6e38cf
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-19 19:44:12 -0400

    still trying to figure out how to make the work in about to fit the about page better

--- src/componets/about/about.jsx
+++ src/componets/about/about.jsx
@@ -43,8 +43,10 @@ const About = () => (
       <h1>Full-Stack Web Development Certificate</h1>
       <img src={certificate} alt="picture of me" />
     </div>
-
-    {/* <Work />  */}
+  <div id='works'>
+  <Work />
+  </div>
+  
   </div>
    
 );
