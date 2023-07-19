dd8dd3e74075e082066165379cc79aadca6a4875
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-18 23:53:53 -0400

    thank god

--- src/componets/about/about.jsx
+++ src/componets/about/about.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import './about.css';
-import me from "./images/PIC.jpg"
-import certificate from "./images/cer.png"
+import me from "./images/PIC.jpg";
+import certificate from "./images/cer.png";
 import skills1 from './images/react.png';
 import skills2 from './images/html.png';
 import skills3 from './images/csss.png';
@@ -11,40 +12,39 @@ import skills7 from './images/moo.png';
 import skills8 from './images/msql.png';
 import skills9 from './images/express.png';
 import skills10 from './images/op.png';
+import Work from '../work/work';
 
-const handleImageClick = (websiteUrl) => {
-    window.location.href = websiteUrl;
-  };
-
-
-const About = () =>
-    <div id="about">
-        <img src={me} alt="picture of me" />
-        <div id='me'>
-        <h1>Bio</h1>
-        <h2> 
+const About = () => (
+  <div id="about">
+    <img src={me} alt="picture of me" />
+    <div id='me'>
+      <h1>Bio</h1>
+      <h2>
         I'm Justin Delgado, a passionate full-stack developer who has honed my skills through rigorous training at UCF's esteemed bootcamp.
-         With a deep love for technology, my ultimate passion lies in bringing ideas to life through web development. <br />
-        </h2>
-        </div>
-    <div id="bg">
-        <div id="skills">
-            <img src={skills1} alt='react image' />
-            <img src={skills2} alt='html image' />
-            <img src={skills3} alt='css image' />
-            <img src={skills4} alt='jss image' />
-            <img src={skills5} alt='node image' />
-            <img src={skills6} alt='sql image' />
-            <img src={skills7} alt='mongoDb image' />
-            <img src={skills8} alt='mysql image' />
-            <img src={skills9} alt='express image' />
-            <img src={skills10} alt='oop image' />
-        </div>
-        </div>
-        <div id="bottom">
-        <h1>Full-Stack Web Development Certificate</h1>
-        <img src={certificate} alt="picture of me" />
-        </div>
+        With a deep love for technology, my ultimate passion lies in bringing ideas to life through web development. <br />
+      </h2>
     </div>
+    <div id="bg">
+      <div id="skills">
+        <img src={skills1} alt='react image' />
+        <img src={skills2} alt='html image' />
+        <img src={skills3} alt='css image' />
+        <img src={skills4} alt='jss image' />
+        <img src={skills5} alt='node image' />
+        <img src={skills6} alt='sql image' />
+        <img src={skills7} alt='mongoDb image' />
+        <img src={skills8} alt='mysql image' />
+        <img src={skills9} alt='express image' />
+        <img src={skills10} alt='oop image' />
+      </div>
+    </div>
+    <div id="bottom">
+      <h1>Full-Stack Web Development Certificate</h1>
+      <img src={certificate} alt="picture of me" />
+    </div>
+
+    <Work /> 
+  </div>
+);
 
 export default About;
--- src/componets/work/work.js
+++ src/componets/work/work.js
@@ -1,5 +1,20 @@
+import React from 'react';
 import './work.css';
 import apps from './work.json';
+
+import pupsImage from './images/pups.jpg';
+import terminatorImage from './images/IMG_2662.PNG';
+import garageJunkiesImage from './images/hutt.png';
+import rockPaperScissorsImage from './images/roc.jpg';
+import weatherTrackerImage from './images/rain.png';
+
+const appImages = {
+  'ThePawPath': pupsImage,
+  'Typo Terminator': terminatorImage,
+  'Garage Junkies': garageJunkiesImage,
+  'Rock,Paper,Scissors': rockPaperScissorsImage,
+  'Weather Tracker': weatherTrackerImage
+};
 
 const Work = () => {
   const handleImageClick = (websiteUrl) => {
@@ -11,10 +26,10 @@ const Work = () => {
       <h1>Recent Projects</h1>
 
       <div id="appsDiv">
-        {apps.map(({ key, appName, pic, info, websiteUrl }) => (
+        {apps.map(({ key, appName, info, websiteUrl }) => (
           <div className="card" key={key}>
             <img
-              src={pic}
+              src={appImages[appName]}
               alt="Picture of the app"
               onClick={() => handleImageClick(websiteUrl)}
             />
