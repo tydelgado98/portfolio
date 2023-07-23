592d09b29b9d18b11f6c2d69e736effdc3a852ba
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-23 19:32:39 -0400

    prototype build

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Nav, Footer, About, Work, Contact } from './componets';
 import { useState } from 'react';
 
 const App = () => {
-  const [page, setPage] = useState('work');
+  const [page, setPage] = useState('about');
 
 return <>
   <Nav setPage = {setPage} />
